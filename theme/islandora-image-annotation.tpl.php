91c587b5226eff6387779e1d05717c8aad0ba8ff
DannyJoris <danny_joris@yahoo.co.uk>
2013-04-05 15:12:49 -0300

    some cleanup

--- theme/islandora-image-annotation.tpl.php
+++ theme/islandora-image-annotation.tpl.php
@@ -6,6 +6,12 @@
 ?>
 
 <div class="islandora-anno-wrapper">
+
+  <div class="islandora-image-annotation">
+    <div class="islandora-image-annotation-annotations">image annotations</div>
+    <div class="islandora-image-annotation-canvas">canvas</div>
+  </div>
+
   <input id="full-window-button" type="button" value="<?php print t('Full Window'); ?>" />
   <div class="islandora-anno-iframe-wrapper">
     <iframe id="islandora-anno-iframe" src="<?php print $islandora_image_annotation_src; ?>" scrolling="0" frameborder="0" style="width: 100%; height: 800px;"><?php print t('Errors: unable to load SharedCanavas'); ?></iframe>
