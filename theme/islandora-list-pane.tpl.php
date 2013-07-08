0a629b2a8f8b32f78807f274d52ce9f928890f30
William Panting <willtp87@hotmail.com>
2013-07-08 11:32:08 -0300

    whitespace

--- theme/islandora-list-pane.tpl.php
+++ theme/islandora-list-pane.tpl.php
@@ -6,7 +6,7 @@
 
 ?>
 
-      <!-- Image Annotations Panel -->
-      <div id="image-annotations">
-        <div id="comment_annos_block" class="dragBlock"></div>
-      </div>
+<!-- Image Annotations Panel -->
+<div id="image-annotations">
+  <div id="comment_annos_block" class="dragBlock"></div>
+</div>
