29aa56b39811f685f36e7a7a5f44096d23c32ee5
MorganDawe <mdawe@discoverygarden.com>
2014-01-02 13:07:40 +0000

    Fixing random color.

--- shared_canvas/stable/sc_ui.js
+++ shared_canvas/stable/sc_ui.js
@@ -892,9 +892,6 @@ function paint_commentAnnoTargets(ttldiv, canvasId, annoId, annoType) {
       } else {
         col = get_random_color();//svgAreaColors.splice(0,1)[0];
       }
-      if(islandora_canvas_params.mappings['urn:uuid:' + annoId] != ''){
-        col = islandora_canvas_params.mappings[['urn:uuid:' + annoId]];
-      }
       if(islandora_canvas_params.islandora_anno_stroke_width != null) {
         strokeWidth = islandora_canvas_params.islandora_anno_stroke_width;
       } else if(islandora_canvas_params.strokeWidth['urn:uuid:' + annoId] != ''){
@@ -904,6 +901,9 @@ function paint_commentAnnoTargets(ttldiv, canvasId, annoId, annoType) {
       }
       if(Drupal.settings.islandora_image_annotation.can_choose == 1) {
           col = get_random_color();
+      }
+      if(islandora_canvas_params.mappings['urn:uuid:' + annoId] != ''){
+        col = islandora_canvas_params.mappings[['urn:uuid:' + annoId]];
       }
       // Fix for google chrome not getting the color.
       if(typeof col == 'undefined'){
